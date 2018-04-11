6cd6647f0158e37c85243cacbf87e73b8497448a
Guilherme <guilherme.prado@lnls.br>
2018-04-11 12:40:42 -0300

    AS PS update to work with new controller classes

--- as-ps/as_ps/as_ps.py
+++ as-ps/as_ps/as_ps.py
@@ -12,11 +12,12 @@ import siriuspy.util as _util
 # import as_ps.pvs as _pvs
 from as_ps.main import App
 from siriuspy.envars import vaca_prefix as _VACA_PREFIX
-from siriuspy.search import PSSearch
-from siriuspy.pwrsupply.data import PSData
-from siriuspy.pwrsupply.pru import PRU
-from siriuspy.pwrsupply.model import FBPPowerSupply
-from siriuspy.pwrsupply.controller import FBPController, FBPControllerSim
+# from siriuspy.search import PSSearch
+# from siriuspy.pwrsupply.data import PSData
+# from siriuspy.pwrsupply.pru import PRU
+# from siriuspy.pwrsupply.model import FBPPowerSupply
+# from siriuspy.pwrsupply.controller import FBPController, FBPControllerSim
+from siriuspy.pwrsupply.beaglebone import BeagleBone as _BeagleBone
 
 INTERVAL = 0.1/10
 stop_event = False  # _multiprocessing.Event()
@@ -34,33 +35,9 @@ def _stop_now(signum, frame):
     stop_event = True
 
 
-def get_devices(bbblist, simulate=True):
+def get_devices(bbbs, simulate=True):
     """Rerturn a controller for each device."""
-    devices = {}
-    for bbbname in bbblist:
-        psnames = PSSearch.conv_bbbname_2_psnames(bbbname)
-        for i, psname in enumerate(psnames):
-            # Define device controller
-            if not simulate:
-                serial = PRU()
-                # Temp fix for test benches, TODO: remove
-                serial_address = {'BO-01U:PS-CH': 1, 'BO-01U:PS-CV': 2,
-                                  'BO-03U:PS-CH': 5, 'BO-03U:PS-CV': 6}
-                if bbbname in ('BO-01:CO-BBB-2', 'BO-01:CO-BBB-1'):
-                    if psname in ('BO-01U:PS-CH', 'BO-01U:PS-CV',
-                                  'BO-03U:PS-CH', 'BO-03U:PS-CV'):
-                        controller = \
-                            FBPController(serial, serial_address[psname])
-                    else:
-                        controller = FBPControllerSim()
-                else:
-                    controller = FBPController(serial, i + 1)
-            else:  # Simulate
-                controller = FBPControllerSim()
-            # Create device and dispatcher
-            db = PSData(psname).propty_database
-            devices[psname] = FBPPowerSupply(controller, database=db)
-    return devices
+    pass
 
 
 def get_database(controllers):
@@ -75,9 +52,9 @@ def get_database(controllers):
 
 class _PCASDriver(_pcaspy.Driver):
 
-    def __init__(self, devices, database):
+    def __init__(self, bbblist, database):
         super().__init__()
-        self.app = App(self, devices, database, _PREFIX)
+        self.app = App(self, bbblist, database, _PREFIX)
 
     def read(self, reason):
         value = self.app.read(reason)
@@ -95,9 +72,19 @@ def run(bbblist, simulate=True):
     # Define abort function
     _signal.signal(_signal.SIGINT, _stop_now)
     _signal.signal(_signal.SIGTERM, _stop_now)
-    # What if serial is no running?
-    devices = get_devices(bbblist, simulate=simulate)
+
+    # Create BBBs
+    bbbs = dict()
+    devices = dict()
+    for bbbname in bbblist:
+        bbb = _BeagleBone(bbbname, simulate)
+        for psname in bbb.psnames:
+            devices[psname] = bbb[psname]
+            bbbs[psname] = bbb
+    # What if serial is not running?
+    # devices = get_devices(bbbs, simulate=simulate)
     database = get_database(devices)
+
     # Check if IOC is already running
     pvname = \
         _PREFIX + list(devices[list(devices)[0]].database)[0]
@@ -106,20 +93,26 @@ def run(bbblist, simulate=True):
     if running:
         print('Another PS IOC is already running!')
         return
+
     # Create a new simple pcaspy server and driver to respond client's requests
     server = _pcaspy.SimpleServer()
     server.createPV(_PREFIX, get_database(devices)[_PREFIX])
+
     # initiate a new thread responsible for listening for client connections
     server_thread = _pcaspy_tools.ServerThread(server)
+
     # Create driver to handle requests
-    pcas_driver = _PCASDriver(devices, database)
+    pcas_driver = _PCASDriver(bbbs, database)
+
     # Create scan thread that'll enqueue read request to update DB
     scan_thread = _Thread(target=pcas_driver.app.enqueue_scan, daemon=True)
+
     # Start threads and processing
     server_thread.start()
     scan_thread.start()
     while not stop_event:
         pcas_driver.app.process(INTERVAL)
+
     # Signal received, exit
     print('exiting...')
     server_thread.stop()
--- as-ps/as_ps/main.py
+++ as-ps/as_ps/main.py
@@ -39,15 +39,16 @@ class App:
 
     Operation = _namedtuple('Operation', 'device, function, kwargs')
 
-    def __init__(self, driver, devices, database, prefix):
+    def __init__(self, driver, bbblist, database, prefix):
         """Create Power Supply controllers."""
-        self._devices = devices
+        # self._devices = devices
+        self._bbblist = bbblist
         self._driver = driver
         self._op_deque = _deque()
         self.scan = True
-        for dev_name, device in devices.items():
-            version = device.read('Version-Cte')
-            reason = dev_name + ':Version-Cte'
+        for psname, bbb in self.bbblist.items():
+            version = bbb[psname].read('Version-Cte')
+            reason = psname + ':Version-Cte'
             driver.setParam(reason, version)
         # Print info about the IOC
         _siriuspy.util.print_ioc_banner(
@@ -68,9 +69,9 @@ class App:
         return self._driver
 
     @property
-    def devices(self):
-        """Devices."""
-        return self._devices
+    def bbblist(self):
+        """BBBs."""
+        return self._bbblist
 
     def process(self, interval):
         """Process method."""
@@ -100,8 +101,9 @@ class App:
 
     def _write(self, device, field, value):
         """Write value to device field."""
+        bbb = self.bbblist[device]
         reason = device + ':' + field
-        if self.devices[device].write(field, value):
+        if bbb.set(device, field, value):
             if isinstance(value, _np.ndarray):
                 print("[{:2s}] - {:32s}".format('W', reason))
             else:
@@ -110,7 +112,7 @@ class App:
                 reason, _Alarm.NO_ALARM, _Severity.NO_ALARM)
         else:
             print("[{:2s}] - {:32s} = {} - SERIAL ERROR".format(
-                'W', reason, value))
+                'W', reason))
             self.driver.setParamStatus(
                 reason, _Alarm.TIMEOUT_ALARM, _Severity.INVALID_ALARM)
         self.driver.setParam(reason, value)
@@ -119,8 +121,19 @@ class App:
 
     def update_db(self, device_name):
         """Read variables and update DB."""
-        dev = self.devices[device_name]
-        vars = dev.read_group(3)
+        bbb = self.bbblist[device_name]
+        dev = bbb[device_name]
+
+        # Get fields
+        fields = dict()
+        fields.update(dev.read_setpoints())
+        fields.update(dev.read_status())
+        for field, value in fields.items():
+            reason = device_name + ':' + field
+            self.driver.setParam(reason, value)
+
+        # Read from serial
+        vars = dev.read_ps_variables()
         conn = dev.connected
         if not conn:
             for field in dev.database:
@@ -137,13 +150,15 @@ class App:
                     reason, _Alarm.NO_ALARM, _Severity.NO_ALARM)
         else:
             print("[RA] - Failed to read {} variables.".format(device_name))
+
+        # Update PVs in DB
         self.driver.updatePVs()
         return
 
     def enqueue_scan(self):
         """Enqueue read methods run as a thread."""
         while self.scan:
-            for psname, device in self.devices.items():
+            for psname in self.bbblist:
                 op = App.Operation(
                     psname, self.update_db, {'device_name': psname})
                 self._op_deque.append(op)
