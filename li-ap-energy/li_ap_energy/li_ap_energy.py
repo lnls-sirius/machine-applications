a4e46ed37fc96e456bc496f73a473fac656bc33e
Fernando <fernandohds564@gmail.com>
2019-07-10 16:46:59 -0300

    ENERGY.WIP: First commit.

---
+++ li-ap-energy/li_ap_energy/li_ap_energy.py
@@ -0,0 +1,180 @@
+"""IOC Module."""
+
+import os as _os
+import time as _time
+import logging as _log
+import signal as _signal
+from threading import Event as _Event
+import pcaspy as _pcaspy
+import pcaspy.tools as _pcaspy_tools
+from as_ti_control import App
+from siriuspy import util as _util
+from siriuspy.csdevice import util as _cutil
+from siriuspy.envars import vaca_prefix as _vaca_prefix
+from siriuspy.search import HLTimeSearch as _HLTimeSearch
+
+__version__ = _util.get_last_commit_hash()
+INTERVAL = 0.5
+stop_event = _Event()
+
+
+def _stop_now(signum, frame):
+    _log.info('SIGINT received')
+    global stop_event
+    stop_event.set()
+
+
+def _attribute_access_security_group(server, db):
+    for k, v in db.items():
+        if k.endswith(('-RB', '-Sts', '-Cte', '-Mon')):
+            v.update({'asg': 'rbpv'})
+    path_ = _os.path.abspath(_os.path.dirname(__file__))
+    server.initAccessSecurityFile(path_ + '/access_rules.as')
+
+
+class _Driver(_pcaspy.Driver):
+
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+        self.app.driver = self
+
+    def read(self, reason):
+        _log.debug("Reading {0:s}.".format(reason))
+        return super().read(reason)
+
+    def write(self, reason, value):
+        if not self._isValid(reason, value):
+            return False
+        ret_val = self.app.write(reason, value)
+        if ret_val:
+            _log.info('YES Write %s: %s', reason, str(value))
+        else:
+            value = self.getParam(reason)
+            _log.warning('NO Write %s: %s', reason, str(value))
+        self.setParam(reason, value)
+        self.updatePV(reason)
+        return True
+
+    def _isValid(self, reason, val):
+        if reason.endswith(('-Sts', '-RB', '-Mon', '-Cte')):
+            _log.debug('PV {0:s} is read only.'.format(reason))
+            return False
+        if val is None:
+            msg = 'client tried to set None value. refusing...'
+            _log.error(msg)
+            return False
+        enums = self.getParamInfo(reason, info_keys=('enums', ))['enums']
+        if enums and isinstance(val, int) and val >= len(enums):
+            _log.warning('value %d too large for enum type PV %s', val, reason)
+            return False
+        return True
+
+
+class _ServerThread(_pcaspy_tools.ServerThread):
+
+    def __init__(self, server, interval=0.1):
+        super().__init__(server)
+        self.interval = interval
+
+    def run(self):
+        """
+        Start the server processing
+        """
+        while self.running:
+            t0 = _time.time()
+            self.server.process(self.interval)
+            dt = _time.time() - t0
+            if dt > self.interval*1.1:
+                _log.info('Process took: {0:.4f} s'.format(dt))
+
+
+def run(timing='trig-all', lock=False, wait=5, debug=False, interval=0.1):
+    """Start the IOC."""
+    _util.configure_log_file(debug=debug)
+    _log.info('Starting...')
+
+    # define abort function
+    _signal.signal(_signal.SIGINT, _stop_now)
+    _signal.signal(_signal.SIGTERM, _stop_now)
+
+    # get IOC name and triggers list
+    if not trig_list:
+        _log.fatal('Must select some triggers to run IOC.')
+        return
+    # Creates App object
+    _log.debug('Creating App Object.')
+    app = App(trig_list=trig_list)
+    db = app.get_database()
+    db[ioc_prefix + ':Version-Cte'] = {'type': 'string', 'value': __version__}
+    # add PV Properties-Cte with list of all IOC PVs:
+    db = _cutil.add_pvslist_cte(db, prefix=ioc_prefix)
+    # check if IOC is already running
+    running = _util.check_pv_online(
+        pvname=_vaca_prefix + sorted(db.keys())[0],
+        use_prefix=False, timeout=0.5)
+    if running:
+        _log.error('Another ' + ioc_name + ' is already running!')
+        return
+    _util.print_ioc_banner(
+            ioc_name, db, 'High Level Timing IOC.', __version__, _vaca_prefix)
+    # create a new simple pcaspy server and driver to respond client's requests
+    _log.info('Creating Server.')
+    server = _pcaspy.SimpleServer()
+    _attribute_access_security_group(server, db)
+    _log.info('Setting Server Database.')
+    server.createPV(_vaca_prefix, db)
+    _log.info('Creating Driver.')
+    _Driver(app)
+
+    if not lock:
+        tm = max(2, wait)
+        _log.info(
+            'Waiting ' + str(tm) + ' seconds to start locking Low Level.')
+        stop_event.wait(tm)
+        _log.info('Start locking now.')
+
+    if not stop_event.is_set():
+        app.locked = True
+
+    # initiate a new thread responsible for listening for client connections
+    server_thread = _ServerThread(server, interval)
+    server_thread.daemon = True
+    _log.info('Starting Server Thread.')
+    server_thread.start()
+
+    # set state
+    db = app.get_database()
+    m2w = app.get_map2writepvs()
+    if lock:
+        for pv, fun in m2w.items():
+            if pv.endswith('-Cmd') or pv.endswith('LowLvlLock-Sel'):
+                continue
+            val = db[pv]['value']
+            fun(val)
+    else:  # or update driver state
+        for pv, fun in app.get_map2readpvs().items():
+            val = fun()
+            value = val.pop('value')
+            if value is None:
+                value = db[pv]['value']
+            if pv.endswith(('-SP', '-Sel')) and not pv.endswith('LvlLock-Sel'):
+                m2w[pv](value)
+            try:
+                app.driver.setParam(pv, value)
+            except TypeError as err:
+                print(pv, value)
+                raise err
+            app.driver.setParamStatus(pv, **val)
+            app.driver.updatePV(pv)
+
+    # main loop
+    while not stop_event.is_set():
+        app.process(INTERVAL)
+
+    _log.info('Stoping Server Thread...')
+    # send stop signal to server thread
+    server_thread.stop()
+    server_thread.join()
+    _log.info('Server Thread stopped.')
+    _log.info('Good Bye.')
