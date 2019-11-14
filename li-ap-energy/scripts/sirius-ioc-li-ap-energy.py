bd73a3b0bb2c3c398496e9caf964a0482305d807
Fernando <fernandohds564@gmail.com>
2019-11-14 09:27:23 -0300

    ENERMEAS.BUG: change environment variable value to string.

--- li-ap-energy/scripts/sirius-ioc-li-ap-energy.py
+++ li-ap-energy/scripts/sirius-ioc-li-ap-energy.py
@@ -6,7 +6,7 @@ from li_ap_energy import run
 
 
 # Linac image is very large! (2448 X 2050)
-os.environ['EPICS_CA_MAX_ARRAY_BYTES'] = 21000000
+os.environ['EPICS_CA_MAX_ARRAY_BYTES'] = '21000000'
 
 parser = _argparse.ArgumentParser(description="Run LI Energy IOC.")
 parser.add_argument(
