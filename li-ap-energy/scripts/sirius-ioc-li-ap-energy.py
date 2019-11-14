611a852e92dc6f284f4b6b745817c0922aa4a02d
Fernando <fernandohds564@gmail.com>
2019-11-14 09:23:13 -0300

    ENERMEAS.BUG: Set EPICS_CA_MAX_ARRAY_BYTES for ioc to work properly.

--- li-ap-energy/scripts/sirius-ioc-li-ap-energy.py
+++ li-ap-energy/scripts/sirius-ioc-li-ap-energy.py
@@ -1,7 +1,12 @@
 #!/usr/local/bin/python-sirius -u
 
+import os
 import argparse as _argparse
 from li_ap_energy import run
+
+
+# Linac image is very large! (2448 X 2050)
+os.environ['EPICS_CA_MAX_ARRAY_BYTES'] = 21000000
 
 parser = _argparse.ArgumentParser(description="Run LI Energy IOC.")
 parser.add_argument(
