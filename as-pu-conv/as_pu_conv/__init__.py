88d07eb8794958ab5be605b36d17e9fc532f8a5c
ximenes <xresende@gmail.com>
2019-12-06 19:18:20 -0300

    Add as-pu-conv IOC

---
+++ as-pu-conv/as_pu_conv/__init__.py
@@ -0,0 +1,3 @@
+"""AS PU Conv package."""
+
+__all__ = ('as_pu_conv', 'main')
---
+++ as-pu-conv/scripts/sirius-ioc-as-pu-conv.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python-sirius -u
+"""BeagleBone Black IOCs Launcher."""
+import sys
+import os
+from as_pu_conv import as_pu_conv as ioc_module
+
+
+# NOTE: maximum epics array size
+os.environ['EPICS_CA_MAX_ARRAY_BYTES'] = '100000'
+
+
+def main():
+    """Launch PU Conv IOC."""
+    args = [arg for arg in sys.argv[1:]]
+    ioc_module.run(args)
+
+
+if __name__ == "__main__":
+    main()
