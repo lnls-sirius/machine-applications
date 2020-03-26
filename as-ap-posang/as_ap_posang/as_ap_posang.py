af4e5bf8c3461e7d6646955ee531a0cd81147cf9
anacso17 <anacso1777@gmail.com>
2020-03-25 21:06:56 -0300

    PosAng.MNT: change App class to derive from CallBack, remove pvs.py module, fix csdevice imports and cleanup code

--- as-ap-posang/as_ap_posang/as_ap_posang.py
+++ as-ap-posang/as_ap_posang/as_ap_posang.py
@@ -5,10 +5,11 @@ import sys as _sys
 import signal as _signal
 import pcaspy as _pcaspy
 import pcaspy.tools as _pcaspy_tools
-from siriuspy import util as _util
-import as_ap_posang.pvs as _pvs
-import as_ap_posang.main as _main
 
+from siriuspy import util as _util
+from siriuspy.envars import VACA_PREFIX as _vaca_prefix
+
+from .main import App as _App
 
 INTERVAL = 0.1
 stop_event = False
@@ -32,9 +33,10 @@ def _attribute_access_security_group(server, db):
 
 class _PCASDriver(_pcaspy.Driver):
 
-    def __init__(self):
+    def __init__(self, app):
         super().__init__()
-        self.app = _main.App(self)
+        self.app = app
+        self.app.add_callback(self.update_pv)
 
     def read(self, reason):
         return super().read(reason)
@@ -45,6 +47,10 @@ class _PCASDriver(_pcaspy.Driver):
         else:
             return False
 
+    def update_pv(self, pvname, value,  **kwargs):
+        self.setParam(pvname, value)
+        self.updatePV(pvname)
+
 
 def run(transport_line, correctors_type='ch-sept'):
     """Run main module function."""
@@ -52,24 +58,35 @@ def run(transport_line, correctors_type='ch-sept'):
     _signal.signal(_signal.SIGINT, _stop_now)
     _signal.signal(_signal.SIGTERM, _stop_now)
 
+    # configure log file
     _util.configure_log_file()
 
-    # define IOC
-    _pvs.select_ioc(transport_line, correctors_type)
-    _main.App.init_class()
-    prefix = _pvs.get_pvs_prefix()
-    db = _main.App.pvs_database
+    # define IOC, init pvs database and create app object
+    _version = _util.get_last_commit_hash()
+    _ioc_prefix = _vaca_prefix + transport_line.upper() + '-Glob:AP-PosAng:'
+    app = _App(transport_line, corrs_type=correctors_type)
+    db = app.pvs_database
+    db['Version-Cte']['value'] = _version
 
     # check if another IOC is running
-    pvname = prefix + next(iter(db))
+    pvname = _ioc_prefix + next(iter(db))
     if _util.check_pv_online(pvname, use_prefix=False):
         raise ValueError('Another instance of this IOC is already running!')
+
+    # check if another IOC is running
+    _util.print_ioc_banner(
+        ioc_name=transport_line.upper() + '-AP-PosAng',
+        db=db,
+        description=transport_line.upper()+'-AP-PosAng Soft IOC',
+        version=_version,
+        prefix=_ioc_prefix)
 
     # create a new simple pcaspy server and driver to respond client's requests
     server = _pcaspy.SimpleServer()
     _attribute_access_security_group(server, db)
-    server.createPV(prefix, db)
-    pcas_driver = _PCASDriver()
+    server.createPV(_ioc_prefix, db)
+    pcas_driver = _PCASDriver(app)
+    app.init_database()
 
     # initiate a new thread responsible for listening for client connections
     server_thread = _pcaspy_tools.ServerThread(server)
