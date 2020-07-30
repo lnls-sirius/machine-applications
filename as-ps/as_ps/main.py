bac3ff176e1c3fc076df60bbd99f858bd68b960d
ximenes <xresende@gmail.com>
2020-07-30 20:28:42 -0300

    Change PS IOC so as to print warning when write is requested in SOFB mode

--- as-ps/as_ps/main.py
+++ as-ps/as_ps/main.py
@@ -49,6 +49,7 @@ class App:
 
         # mapping device to bbb
         self._bbblist = bbblist
+        self._sofbmode_sts_pvname = self.bbblist[0].psnames[0] + ':SOFBMode-Sts'
 
         # build dictionaries
         self._dev2bbb, self._dev2conn, self._interval = \
@@ -117,8 +118,13 @@ class App:
         # print('{:<30s} : {:>9.3f} ms'.format(
         #     'IOC.write (beg)', 1e3*(_time.time() % 1)))
         pvname = _SiriusPVName(reason)
-        _log.info("[{:.2s}] - {:.32s} = {:.50s}".format(
-            'W ', reason, str(value)))
+
+        sofb_state = self.driver.getParam(self._sofbmode_sts_pvname)
+        ignorestr, wstr = \
+            (' (SOFBMode On)', 'W!') if sofb_state and 'SOFB' not in reason else ('', 'W ')
+
+        _log.info("[{:.2s}] - {:.32s} = {:.50s}{}".format(
+            wstr, reason, str(value), ignorestr))
 
         # NOTE: This modified behaviour is to allow loading
         # global_config to complete without artificial warning
