d6c2abfd16595845c4c2bccf604c42dc91ec8335
Ana Clara Oliveira <ana.clara@lnls.br>
2023-10-20 17:03:59 -0300

    ORBINTLK.FIX: fix bug in script

--- si-ap-orbintlk/scripts/sirius-ioc-si-ap-orbintlk.py
+++ si-ap-orbintlk/scripts/sirius-ioc-si-ap-orbintlk.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python-sirius
 """High Level FOFB IOC executable."""
 
-from si_ap_fofb import si_ap_fofb as ioc_module
+from si_ap_orbintlk import si_ap_orbintlk as ioc_module
 ioc_module.run()
