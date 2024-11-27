86f531a617e37522b70badaf0c31cbd17cd1d4fc
Ximenes Rocha Resende <xresende@gmail.com>
2024-11-27 15:06:51 -0300

    Change IDFF IOC for Trim correctors

--- si-ap-idff/scripts/sirius-ioc-si-ap-idff.py
+++ si-ap-idff/scripts/sirius-ioc-si-ap-idff.py
@@ -8,7 +8,21 @@ from si_ap_idff import si_ap_idff as ioc_module
 
 def main():
     """Launch IOC."""
-    ioc_module.run(sys.argv[1])
+    idname = sys.argv[1]
+
+    # select by idname what correctors belong to IDFF
+    kwargs = dict(
+        enbl_chcorrs=False, enbl_cvcorrs=False,
+        enbl_qscorrs=False, enbl_lccorrs=False,
+        enbl_qdcorrs=False)
+    if idname in ('SI-08SB:ID-IVU18', 'SI-14SB:ID-IVU18'):
+        kwargs.update(
+            dict(enbl_qdcorrs=True))
+    elif idname in ('SI-10SB:ID-DELTA52', 'SI-10SB:ID-EPU50'):
+        kwargs.update(
+            dict(enbl_chcorrs=True, enbl_cvcorrs=True, enbl_qscorrs=True))
+
+    ioc_module.run(sys.argv[1], **kwargs)
 
 
 if __name__ == "__main__":
