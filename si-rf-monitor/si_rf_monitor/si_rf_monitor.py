655b3866c1da794da9a4d53f9038f0def3564d34
Fernando <fernandohds564@gmail.com>
2025-08-06 16:23:25 -0300

    SIRFMON.ENH: Add some tools.

--- si-rf-monitor/si_rf_monitor/si_rf_monitor.py
+++ si-rf-monitor/si_rf_monitor/si_rf_monitor.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python-sirius
+import logging as _log
+import os as _os
+import signal as _signal
 
 import pcaspy as _pcaspy
 import pcaspy.tools as _pcaspy_tools
-import signal as _signal
+
+from siriuspy import util as _util
+from siriuspy import csdev as _csdev
+
 from . import main as _main
 from . import pvs as _pvs
 
 INTERVAL = 0.1
 stop_event = False
+__version__ = _util.get_last_commit_hash()
 PREFIX = ''
 
 
@@ -15,6 +22,14 @@ def _stop_now(signum, frame):
     print(' - SIGNAL received.')
     global stop_event
     stop_event = True
+
+
+def _attribute_access_security_group(server, db):
+    for k, v in db.items():
+        if k.endswith(('-RB', '-Sts', '-Cte', '-Mon')):
+            v.update({'asg': 'rbpv'})
+    path_ = _os.path.abspath(_os.path.dirname(__file__))
+    server.initAccessSecurityFile(path_ + '/access_rules.as')
 
 
 class _PCASDriver(_pcaspy.Driver):
@@ -52,14 +67,34 @@ def run():
     _signal.signal(_signal.SIGINT, _stop_now)
     _signal.signal(_signal.SIGTERM, _stop_now)
 
+    _util.configure_log_file(debug=False)
+    _log.info('Starting...')
+
     # create the application model
     app = _main.App()
-
     db = app.get_database()
+    db.update({'Version-Cte': {'type': 'string', 'value': __version__}})
+
+    ioc_prefix = _pvs.IOC_PREFIX
+    ioc_name = 'si-rf-monitor'
+
+    # check if IOC is already running
+    running = _util.check_pv_online(
+        pvname=ioc_prefix + sorted(db.keys())[0],
+        use_prefix=False, timeout=0.5)
+    # add PV Properties-Cte with list of all IOC PVs:
+    db = _csdev.add_pvslist_cte(db)
+    if running:
+        strf = f'Another {ioc_name} is already running!'
+        _log.error(strf)
+        return
+    _util.print_ioc_banner(
+        ioc_name, db, 'Monitor Criomodule Temps.', __version__, ioc_prefix
+    )
 
     # create a new simple pcaspy server and driver to respond client's requests
     server = _pcaspy.SimpleServer()
-    server.createPV(_pvs.IOC_PREFIX, db)
+    server.createPV(ioc_prefix, db)
 
     # create the driver
     pcas_driver = _PCASDriver(app)
